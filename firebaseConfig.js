745e69bfce02cf870cd02a7c02f542f3f3891815
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2025-04-19 13:27:57 -0700

    added firebaseconfig file

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbdqz-ao2cwZ9CtwK0kfMkZVw5XaLefTo",
+  authDomain: "hackdavis25.firebaseapp.com",
+  projectId: "hackdavis25",
+  storageBucket: "hackdavis25.firebasestorage.app",
+  messagingSenderId: "684707979443",
+  appId: "1:684707979443:web:60784abf53cd97d53f10c6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
