a585b0bbe2c646e79ab70603aa7a752107518246
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2025-04-19 16:45:45 -0700

    firebaseconfig

--- flask-server/firebase_config.py
+++ flask-server/firebase_config.py
@@ -1,9 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def init_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("../serviceAccountKey.json")
+        cred_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+        cred = credentials.Certificate(cred_path)
         firebase_admin.initialize_app(cred)
     return firestore.client()
