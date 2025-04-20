e7b632ab67d55213127567bde15c1eb2ab3ee150
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2025-04-20 10:25:45 -0700

    fixed sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,30 +1,36 @@
+"use client";
+
 import type React from "react";
 import "@/app/globals.css";
 import { Inter, Work_Sans } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import { AccountSidebar } from "@/components/Sidebar"; // Import the sidebar
 import { SidebarProvider } from "@/components/ui/sidebar"; // Import the provider
+import { useIsExpanded } from "@/lib/atom";
 
 const inter = Work_Sans({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Fourth & Hope Staff Dashboard",
-  description: "Internal dashboard for Fourth & Hope staff members",
-};
-
-export default function DashboardLayout({ // Renamed for clarity
+export default function DashboardLayout({
+  // Renamed for clarity
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isExpanded, setIsExpanded] = useIsExpanded();
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
           <SidebarProvider>
-            <div className="ml-4 flex min-h-screen">
+            <div className="absolute top-0 left-0 h-full">
               <AccountSidebar />
-              <main className="flex-1 bg-gray-50 dark:bg-gray-900"> {/* Ensure main content takes remaining space */}
+              <main
+                className={` bg-gray-50 dark:bg-gray-900 ${
+                  isExpanded ? "ml-64" : "ml-20"
+                }`}
+              >
+                {" "}
+                {/* Ensure main content takes remaining space */}
                 {children}
               </main>
             </div>
